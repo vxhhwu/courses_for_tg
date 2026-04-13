53dc7ec752af1dabdc2dd2f10879c35e2b70ec74
success <tair.flyaway@mail.ru>
2026-04-13 17:02:43 +0300

    Update run.py

--- big_prog/run.py
+++ big_prog/run.py
@@ -4,38 +4,30 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from app.handlers import router
-from app.db_reg import init_db
+from app.db_reg import init_db as init_reg
+from app.db_courses import init_db as init_courses
+from app.db_enroll import init_db as init_enroll
 
 async def startup(dispatcher: Dispatcher):
-    await init_db()
-    print("База данных инициализирована")
+    await init_reg()
+    await init_courses()
+    await init_enroll()
+    print("Базы данных инициализированы")
 
 async def shutdown(dispatcher: Dispatcher):
     print("Бот остановлен")
 
 async def main():
-    load_dotenv()
-    bot = Bot(token='TOKEN')
+    load_dotenv()  # загружает переменные из .env (локально)
+    bot = Bot(token=os.getenv('TOKEN'))  # берёт токен из переменной окружения
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
-
     dp.startup.register(startup)
     dp.shutdown.register(shutdown)
-
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('')
-        print('')
-        print('БОТ ВЫКЛЮЧЕН')
-        print('БОТ ВЫКЛЮЧЕН')
-        print('БОТ ВЫКЛЮЧЕН')
-        print('')
-        print('')
-
-# "f:\PyCharm 2025.1.1.1\бот\big_prog\.venv\Scripts\Activate.ps1" для входа
-# cd "F:\PyCharm 2025.1.1.1\бот\big_prog" для входа
-# python run.py запуск
+        print("\nБот выключен")
